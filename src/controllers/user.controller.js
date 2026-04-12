a4b79da2af813a57119101da7205ef6e11b2c08c
Priyanshu Pratik <pratikpriyanshu004@gmail.com>
2026-04-12 15:59:01 +0530

    wrote auth middleware and updated user controller for access adn refresh tokens with login and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js" 
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+//writing a controller for registering user
 const registerUser = asyncHandler( async (req, res) => {
     const {username, email, fullName, password, } = req.body
 
@@ -90,4 +91,88 @@ const registerUser = asyncHandler( async (req, res) => {
             )
 })
 
-export {registerUser}
+//writing a controller for login
+const loginUser = asyncHandler (async (req, res) => {
+    const {email, username, password} = req.body;
+    if(!(username || email)){
+        throw new ApiError(
+            400,
+            "Username or Email is required "
+        )
+    }
+    const user = await User.findOne({
+        $or: [{email}, {username}]
+    })
+    if(!user){
+        throw new ApiError(
+            404,
+            "User not found"
+        )
+    }
+    
+    //checking password if user exists
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if(!isPasswordValid){
+        throw new ApiError(
+            401,
+            "Invalid user credentials"
+        )
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+    const loggedInUser = await User.findById(user._id).select('-password -refreshToken')
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+            .status(200)
+            .cookie("accessToken", accessToken, options)
+            .cookie("refreshToken", refreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        user: loggedInUser, accessToken, refreshToken
+                    },
+                    "User logged in successfully"
+                )
+            )
+
+})
+
+//writing a controller for generating access and refresh tokens
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({
+            validateBeforeSave: false,
+        });
+        return {
+            accessToken,
+            refreshToken,
+        }
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "something went wrong while generating tokens"
+        )
+    }
+}
+
+//writing a controller for logging out user
+const logout = asyncHandler (async (req, res) => {
+    //remove cookies
+    //reset access token and refresh token
+
+    
+}) 
+export {
+    registerUser,
+    loginUser
+}
